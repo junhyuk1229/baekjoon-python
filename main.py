676b53129afe313d33dc46897f03f5f9e8389e9f
junhyuk1229 <junhyuk1229@gmail.com>
2023-02-19 19:29:07 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,22 @@
 import sys
 
 
-def bubble_sort(input_list, sort_num):
-    for sort_index in range(len(input_list) - 1):
-        for start_index in range(sort_index + 1):
-            if input_list[sort_index - start_index] < input_list[sort_index - start_index + 1]:
-                temp_num = input_list[sort_index - start_index]
-                input_list[sort_index - start_index] = input_list[sort_index - start_index + 1]
-                input_list[sort_index - start_index + 1] = temp_num
-                sort_num -= 1
-            if sort_num == 0:
-                return
-
-
 def main():
-    input_len = int(sys.stdin.readline().rstrip())
-    input_list = list(map(int, sys.stdin.readline().rstrip().split(sep=' ')))
-    sort_num = int(sys.stdin.readline().rstrip())
-
-
-
+    first_str = sys.stdin.readline().rstrip()
+    second_str = sys.stdin.readline().rstrip()
+    check_list = [0] * len(first_str)
+    for temp_char in second_str:
+        temp_list = [0] * len(first_str)
+        for temp_index, temp_temp_char in enumerate(first_str):
+            if temp_char != temp_temp_char:
+                continue
+            if temp_index > 0:
+                temp_list[temp_index] = max(check_list[:temp_index])
+            else:
+                temp_list[temp_index] = 0
+            temp_list[temp_index] += 1
+        print(check_list)
+    print(max(check_list))
     return
 
 
