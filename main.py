9f5d715d4825fcca958dee5800d3286e3b678a96
junhyuk1229 <junhyuk1229@gmail.com>
2023-02-27 21:39:31 +0900

    update main function

--- main.py
+++ main.py
@@ -1,7 +1,57 @@
+from collections import deque
 import sys
 
 
+def dfs_node(input_map: list, check_list: list, temp_deque: deque) -> bool:
+    if len(temp_deque) >= 5:
+        return True
+
+    temp_node = temp_deque[-1]
+
+    for temp_index, temp_map in enumerate(input_map[temp_node]):
+        if not temp_map or check_list[temp_index]:
+            continue
+
+        temp_deque.append(temp_index)
+        check_list[temp_index] = True
+        if dfs_node(input_map, check_list, temp_deque):
+            return True
+        temp_deque.pop()
+        check_list[temp_index] = False
+    return False
+
+
 def main() -> None:
+    # Get data input + make node map
+    input_num, input_len = map(int, sys.stdin.readline().rstrip().split(sep=' '))
+    input_list = [[False] * input_num for _ in range(input_num)]
+    for _ in range(input_len):
+        from_node, to_node = map(int, sys.stdin.readline().rstrip().split(sep=' '))
+        input_list[from_node][to_node] = input_list[to_node][from_node] = True
+    '''
+    print(f"input_num: {input_num}")
+    print(f"input_len: {input_len}")
+    print("input_list:")
+    for temp in input_list:
+        print(temp)
+    print()
+    '''
+
+    # Call dfs for each node as a starting point
+    # If a path with length that is greater than 5 is found print 1
+
+    # Set up variables
+    check_list = [False] * input_num
+    temp_deque = deque()
+    for start_node in range(input_num):
+        temp_deque.append(start_node)
+        check_list[start_node] = True
+        if dfs_node(input_list, check_list, temp_deque):
+            print(1)
+            return
+        temp_deque.pop()
+        check_list[start_node] = False
+    print(0)
 
     return
 
--- temp.py
+++ temp.py
@@ -1,85 +1,73 @@
 import sys
-from collections import deque, defaultdict
 
 
-DX = [1, -1, 0, 0]
-DY = [0, 0, 1, -1]
+def sum_list(input_list: list, sum_num: int) -> None:
+    for temp_index in range(len(input_list) - 1):
+        input_list[temp_index] += sum_num
+    return
 
 
-def change_int(input_height, pos_list):
-    return pos_list[0] * input_height + pos_list[1]
+def sub_list(input_list: list, sub_num: int) -> None:
+    for temp_index in range(len(input_list) - 1):
+        input_list[temp_index] -= sub_num
+    return
 
 
-def bfs_coin_move(input_height, input_width, input_list, bfs_queue, pos_dict):
-    output_list = deque()
-    while bfs_queue:
-        first_coin_pos, second_coin_pos = bfs_queue.pop()
-        for dx, dy in zip(DX, DY):
-            first_h = first_coin_pos[0]
-            first_w = first_coin_pos[1]
-            second_h = second_coin_pos[0]
-            second_w = second_coin_pos[1]
-            fall_num = 2
-            change_flag = False
-
-            if 0 <= first_h + dx < len(input_list) and 0 <= first_w + dy < len(input_list[0]):
-                fall_num -= 1
-                if input_list[first_h + dx][first_w + dy] != '#':
-                    first_h += dx
-                    first_w += dy
-                    change_flag = True
-            if 0 <= second_h + dx < len(input_list) and 0 <= second_w + dy < len(input_list[0]):
-                fall_num -= 1
-                if input_list[second_h + dx][second_w + dy] != '#':
-                    second_h += dx
-                    second_w += dy
-                    change_flag = True
-            if fall_num == 1:
-                return deque()
-            elif fall_num == 2 or not change_flag:
-                continue
-
-            if first_h == second_h and first_w == second_w:
-                continue
-            pos_dict[change_int(input_height, [first_h, first_w])][change_int(input_height, [first_h, first_w])] = True
-            output_list.append([[first_h, first_w], [second_h, second_w]])
-    return output_list
+def check_list(input_map: list, check_list: list, check_len: int) -> bool:
+    # check if check_list matches with the input_map
+    for temp_index in range(check_len - 1):
+        if check_list[temp_index] >= 0 and input_map[temp_index][check_len - 1] == '-':
+            return False
+        if check_list[temp_index] == 0 and input_map[temp_index][check_len - 1] == '0':
+            return False
+        if check_list[temp_index] <= 0 and input_map[temp_index][check_len - 1] == '+':
+            return False
+    return True
 
 
-def loop_coin_move(input_height, input_width, input_list, coin_pos):
-    output_num = 0
-    bfs_queue = deque([coin_pos])
-    pos_dict = defaultdict(lambda: defaultdict(bool))
-    pos_dict[change_int(input_height, coin_pos[0])][change_int(input_height, coin_pos[1])] = True
-    while output_num <= 10:
-        bfs_queue = bfs_coin_move(input_height, input_width, input_list, bfs_queue, pos_dict)
-        if not bfs_queue:
-            return -1
-        output_num += 1
-    if output_num > 10:
-        return -1
-    return output_num
+def loop_comb_num(input_map: list, input_list: list, output_list: list, start_index=0) -> bool:
+    if start_index == len(input_list):
+        return True
+
+    if input_list[start_index] == '-':
+        for temp_num in range(-1, -11, -1):
+            output_list.append(temp_num)
+            if loop_comb_num(input_map, input_list, output_list, start_index + 1):
+                return True
+            output_list.pop()
+    elif input_list[start_index] == '0':
+        output_list.append(0)
+        if loop_comb_num(input_map, input_list, output_list, start_index + 1):
+            return True
+        output_list.pop()
+    else:
+        for temp_num in range(1, 11):
+            output_list.append(temp_num)
+            if loop_comb_num(input_map, input_list, output_list, start_index + 1):
+                return True
+            output_list.pop()
+
+    return False
 
 
-def main():
-    input_height, input_width = map(int, sys.stdin.readline().rstrip().split(sep=' '))
-    input_list = []
-    coin_pos = []
-    for temp_height in range(input_height):
-        input_list.append(sys.stdin.readline().rstrip())
-        for temp_width in range(input_width):
-            if input_list[temp_height][temp_width] != 'o':
-                continue
-            coin_pos.append([temp_height, temp_width])
-    print(coin_pos)
-    for temp_list in input_list:
-        print(temp_list)
+def main() -> None:
+    input_size = int(sys.stdin.readline().rstrip())
+    temp_input = sys.stdin.readline().rstrip()
+    input_map = []
+    start_index = 0
+    for temp_d in range(input_size, 0, -1):
+        input_map.append(' ' * (input_size - temp_d) + temp_input[start_index:start_index + temp_d])
+        start_index += temp_d
+    # print(input_map)
 
-    output_num = loop_coin_move(input_height, input_width, input_list, coin_pos)
-    print(output_num)
+    input_list = [input_map[temp_index][temp_index] for temp_index in range(input_size)]
+    output_list = []
+    loop_comb_num(input_map, input_list, output_list)
+
+    print(' '.join(map(str, output_list)))
 
     return
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
